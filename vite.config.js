38fdda888a351fd5297fbc4c328836a10ac12f7f
tamatm11 <programingtom31072006@ggmail.com>
2025-12-02 22:41:35 +0700

    Fix build scripts and suppress warnings

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 /** @format */
 
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
@@ -11,5 +10,9 @@ export default defineConfig({
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
+  },
+  // Thêm đoạn này để fix cảnh báo vàng
+  build: {
+    chunkSizeWarningLimit: 1000, // Tăng giới hạn cảnh báo lên 1000kb (1MB)
   },
 });
