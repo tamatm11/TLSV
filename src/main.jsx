b90a2f277cd4a0dd8aec770d6d6c573058a37bad
tamatm11 <programingtom31072006@ggmail.com>
2025-12-02 14:32:48 +0700

    Update code

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
 import "@/index.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
